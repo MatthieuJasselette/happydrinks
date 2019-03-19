5dcccb9ac36efd502c31192a483e65e316db394a
matthieu <matthieu.jasselette@gmail.com>
2019-03-19 16:06:40 +0100

    Add randomPseudo

--- src/_components/App.js
+++ src/_components/App.js
@@ -4,27 +4,49 @@ import React, { Component } from 'react';
 import logo from '../_assets/logo.svg';
 import '../_css/App.css';
 import {establishments} from './_establishments/fixtures'
+import Establishment from './_establishments/Establishment'
 
 class App extends Component {
+  constructor (props){
+    super (props);
+    this.state = {
+      pseudo : "Inconnu"
+    }
+  }
+
+  randomPseudo = () => {
+    let randomPseudo    = ""
+    const possible      = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    const size          = Math.floor(Math.random() * 10) + 5
+    for( let i=0; i < size; i++ ){
+        randomPseudo += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
+    //  Update the state via setState() inherited from Component class
+    this.setState({
+        pseudo : randomPseudo
+    })
+  }
+
   render() {
     const listEstablishment = establishments.map( (establishment) => {
-      return (
-        <li
-          key = {establishment.id}
-          className = 'establishment'
-        >
-          <h3>{establishment.name}</h3>
-        </li>
-      )
+        return (
+            <Establishment
+                key={ establishment.id }
+                establishment={ establishment }
+            />
+        )
     })
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to {this.props.title}</h2>
+          <img src={ logo } className="App-logo" alt="logo" />
+          <h2>Welcome "{ this.state.pseudo }" to { this.props.title }</h2>
         </div>
         <div className="App-intro">
-          {listEstablishment}
+          <p> <a onClick={ this.randomPseudo } >Changer le pseudo !</a> </p>
+          <section>
+            { listEstablishment }
+          </section>
         </div>
       </div>
     );
---
+++ src/_components/_establishments/Establishment.js
@@ -0,0 +1,20 @@
+// File path : ./src/_components/_establishments/Establishment.js
+
+import React, { Component } from 'react'
+
+class Establishment extends Component {
+
+    render() {
+
+        return (
+            <div className='establishment' >
+                <h3>{ this.props.establishment.name }</h3>
+
+                { this.props.establishment.description }
+
+            </div>
+        );
+    }
+}
+
+export default Establishment;
