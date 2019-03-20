58e03eaf88752a1fa9db8503d7ebed7369c38cc5
matthieu <matthieu.jasselette@gmail.com>
2019-03-20 11:42:27 +0100

    Updates

---
+++ precache-manifest.be9a8bcc6779c9b400f1304bcab8d1f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "03db67443be58110b7a4",
+    "url": "/static/js/main.e192888c.chunk.js"
+  },
+  {
+    "revision": "157f51664789baf60b64",
+    "url": "/static/js/2.1279535a.chunk.js"
+  },
+  {
+    "revision": "03db67443be58110b7a4",
+    "url": "/static/css/main.2d6c95f9.chunk.css"
+  },
+  {
+    "revision": "e6b4259719c26471d952af04cb729dea",
+    "url": "/index.html"
+  }
+];
